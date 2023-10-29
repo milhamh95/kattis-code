b9f2a1e78f337323e980ca6b8fa5ded719b33669
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-10-29 09:55:41 +0700

    feat(CD): add refactor solution

--- CD/cd.py
+++ CD/cd.py
@@ -1,20 +1,68 @@
 from sys import stdin
 
-cds = stdin.readline().split()
-jackcds = int(cds[0])
-jillcds = int(cds[1])
-
-jackcdsset = set()
-for _ in range(jackcds):
-    jackcdsset.add(int(stdin.readline()))
-
-jillcdsset = set()
-for _ in range(jillcds):
-    jillcdsset.add(int(stdin.readline()))
-
+# Solution 4
+input = stdin.readline
 counter = 0
-for num in jackcdsset:
-    if num in jillcdsset:
-        counter += 1
+while True:
+    n, m = map(int, input().split())
+    if n == 0 and m == 0:
+        break
 
-print(counter)
+    jack = {int(input()) for _ in range(n)}
+    jill = {int(input()) for _ in range(m)}
+    print(len(jack.intersection(jill)))
+
+# Solution 3
+# input = stdin.readline
+# counter = 0
+# while True:
+#     n, m = map(int, input().split())
+#     if n == 0 and m == 0:
+#         break
+#
+#     jack = {int(input()) for _ in range(n)}
+#     counter = sum(1 for _ in range(m) if int(input()) in jack)
+#     print(counter)
+
+# Solution 2
+# Time complexity -> O(n)
+# Space complexity -> O(n)
+# input = stdin.readline
+# while True:
+#     n, m = map(int, input().split())
+#     if n == 0 and m == 0:
+#         break
+#
+#     jackcdsset = set()
+#     for _ in range(n):
+#         jackcdsset.add(int(input()))
+#
+    # counter = 0
+    # for _ in range(m):
+    #     cd = int(input())
+    #     if cd in jackcdsset:
+    #         counter += 1
+#
+#     print(counter)
+
+# Solution 1
+# Time complexity -> O(n)
+# Space complexity -> O(n)
+# cds = stdin.readline().split()
+# jackcds = int(cds[0])
+# jillcds = int(cds[1])
+#
+# jackcdsset = set()
+# for _ in range(jackcds):
+#     jackcdsset.add(int(stdin.readline()))
+#
+# jillcdsset = set()
+# for _ in range(jillcds):
+#     jillcdsset.add(int(stdin.readline()))
+#
+# counter = 0
+# for num in jackcdsset:
+#     if num in jillcdsset:
+#         counter += 1
+#
+# print(counter)
